3efcaa7cd24699b481208515357e9e6568dd6cfd
amelia949 <amellali949@gmail.com>
2015-01-23 00:50:07 -0600

    Hola Mundo

--- HolaMundo/src/com/ejemplo/HolaMundo.java
+++ HolaMundo/src/com/ejemplo/HolaMundo.java
@@ -1,6 +1,10 @@
 package com.ejemplo;
 
 public class HolaMundo {
+	/**
+	 * Implementacion de la aplicacion
+	 * @param args
+	 */
 public static void main(String[] args) {
 	System.out.println("Hola Mundo");
 }
